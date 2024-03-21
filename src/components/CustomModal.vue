8d5c5f766878e3c5d2d5d2f90f844a02bf12bafb
Rizky Fadillah <fadillahrizki16@gmail.com>
2024-03-22 04:45:45 +0700

    add custom modal

---
+++ src/components/CustomModal.vue
@@ -0,0 +1,13 @@
+<script setup>
+defineProps(['content'])
+</script>
+
+<template>
+  <div class="w-full h-full fixed inset-0 flex items-center justify-center">
+    <div class="relative p-4 w-full max-w-md max-h-full">
+      <div class="relative bg-white rounded-lg shadow p-5">
+        <slot></slot>
+      </div>
+    </div>
+  </div>
+</template>
